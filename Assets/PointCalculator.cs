2e3c75832bf01d051c3773aec328965422fce264
shuya matsunobe <tonn5nura32@gmail.com>
2017-05-21 23:24:43 +0900

    lesson5課題
    PinBallに点数機能を付与しました。
    発展課題は未着手です。

---
+++ Assets/PointCalculator.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PointCalculator : MonoBehaviour {
+    private int SmallStarPoint = 10;
+    private int LargeStarPoint = 20;
+    private int SmallCloudPoint = 5;
+    private int LargeCloudPoint = 15;
+    private int PointSum = 0;
+    private int Count = 0;
+
+    private GameObject pointText;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (this.Count == 1)
+        {
+            if (tag == "SmallStarTag")
+            {
+                PointManager.pointSum += SmallStarPoint;
+            }else if (tag == "LargeStarTag")
+            {
+                PointManager.pointSum += LargeStarPoint;
+            }else if(tag == "SmallCloudTag")
+            {
+                PointManager.pointSum += SmallCloudPoint;
+            }else if (tag == "LargeCloudTag")
+            {
+                PointManager.pointSum += LargeCloudPoint;
+            }
+            this.Count=0;    
+        }
+        
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        this.Count=1;
+    }
+}
---
+++ Assets/PointManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PointManager : MonoBehaviour {
+
+    public static int pointSum = 0;
+    public GameObject pointText;
+	// Use this for initialization
+	void Start () {
+        this.pointText = GameObject.Find("PointText");
+        this.pointText.GetComponent<Text>().text = "0";
+    }
+
+    // Update is called once per frame
+    void Update () {
+        this.pointText.GetComponent<Text>().text = pointSum.ToString();
+    }
+}
